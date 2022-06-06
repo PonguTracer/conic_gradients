0ec90c840f3ed078be6ed9317ede5cea2e803a49
Louis Chavez <67764701+PonguTracer@users.noreply.github.com>
2022-06-06 13:15:21 -0700

    Add files via upload
    Creates gradient border with mouse over tracking

---
+++ script.js
@@ -0,0 +1,10 @@
+const card = document.querySelector('.card');
+window.addEventListener('mousemove', ({ clientX, clientY }) => {
+  const { x, y, width, height } = card.getBoundingClientRect();
+  const dx = clientX - (x + 0.5 * width);
+  const dy = clientY - (y + 0.5 * height);
+  const angle = Math.atan2(dy, dx) * 180 / Math.PI;
+  
+  card.style.setProperty('--startDeg', `${angle + 90}deg`);
+  
+}, false)
